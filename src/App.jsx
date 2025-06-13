9e89804fc2972331481d158a27379f1c5215bba6
Jasper Tabin <tabinjasper@gmail.com>
2025-06-13 18:48:35 +0800

    Changed welcome

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Navbar } from './components/Navbar'
 import { MobileMenu } from './components/MobileMenu'
 import { Socials } from './components/Socials'
 
-import { Welcome } from './components/welcome'
+import { Welcome } from './components/Welcome'
 import { Home } from './components/sections/Home'
 import { About } from './components/sections/About'
 import { Skills } from './components/sections/Skills'
